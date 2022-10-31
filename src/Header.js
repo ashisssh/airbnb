a61dbe02bb9d5c1cf2c0061b30bcdf5a383777eb
ashisssh <ashish91030009@gmail.com>
2022-10-31 17:33:38 +0530

    Updated Header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Avatar } from '@material-ui/core';
@@ -12,11 +11,7 @@ function Header() {
         src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg" 
         alt=""
         />
-        <div className='header_center'>
-            <input type= "text" />
-            <SearchIcon />
-        </div>
-
+   
         <div className='header_right'>
             <p>Become a post</p>
             <LanguageIcon />
